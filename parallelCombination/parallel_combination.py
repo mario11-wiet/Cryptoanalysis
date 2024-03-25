96af967aaaf2b59e6fd4f18e576d37b0836719ff
Mariusz Kadziela <mkadziela@oanda.com>
2024-03-25 22:27:36 +0100

    add password genearator algorithm

---
+++ parallelCombination/parallel_combination.py
@@ -0,0 +1,25 @@
+import random
+
+from rule.rule import PasswordRules
+
+
+class ParallelCombination:
+    def __init__(self):
+        self.password_rules = PasswordRules()
+
+    def generate_password(self, length, num_rules_to_apply=3):
+        password = ''
+        for _ in range(length):
+            chosen_rules = random.sample(self.password_rules.rules, num_rules_to_apply)
+            transformed_characters = [rule(password) for rule in chosen_rules]
+            password += ''.join(transformed_characters)
+            self.update_rules_weight(chosen_rules)
+        return password
+
+    def update_rules_weight(self, chosen_rules):
+        for rule in chosen_rules:
+            rule.weight += 1
+
+        total_weight = sum(rule.weight for rule in self.password_rules.rules)
+        for rule in self.password_rules.rules:
+            rule.weight /= total_weight
